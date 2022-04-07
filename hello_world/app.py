04ca2e86c61e7082d57c811c0e9ef1bdcd93c7bd
ChittibabuAngireddi <c.babu.angireddi@accenture.com>
2022-04-08 01:10:22 +0530

    test final - hopefully

--- hello_world/app.py
+++ hello_world/app.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "Test Message1",
+                "message": "Test Message",
                 # "location": ip.text.replace("\n", "")
             }
         ),
