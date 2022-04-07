0ade31898c747afb7132ee5a9f7740a5d5df2863
ChittibabuAngireddi <c.babu.angireddi@accenture.com>
2022-04-08 00:44:35 +0530

    fintest

--- hello_world/app.py
+++ hello_world/app.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world test1",
+                "message": "hello world test",
                 # "location": ip.text.replace("\n", "")
             }
         ),
